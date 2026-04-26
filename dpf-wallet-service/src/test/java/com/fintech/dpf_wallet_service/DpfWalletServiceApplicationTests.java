ea80dadab4bfa6da4f5544d95bd6ef32081abbbc
Sam-TN <meysamtaherinia76@gmail.com>
2026-04-26 14:26:19 +0330

    first commit

---
+++ dpf-wallet-service/src/test/java/com/fintech/dpf_wallet_service/DpfWalletServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fintech.dpf_wallet_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DpfWalletServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
